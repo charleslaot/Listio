fc6084480e085b3ad3ea921b9aeb4e386d5e98dc
Matthew Prestifilippo <mattp@aweber.com>
2018-06-04 21:41:25 -0400

    De-couple render with business logic

--- public/edit.js
+++ public/edit.js
@@ -1,21 +1,56 @@
 'use strict'
 
+const DATA = {
+    results: [],
+    playlist: [],
+    playlistId: location.pathname.split('/')[2]
+};
+
+function emit(event, payload) {
+    console.log(event, payload);
+    switch (event) {
+        case 'search':
+            DATA.results = payload;
+            displaySearchTracks();
+            break;
+        case 'add':
+            console.log('added');
+            break;
+        case 'delete':
+            console.log('deleted');
+            getTracks(DATA.playlistId);
+            break;
+        case 'fetch':
+            console.log('fetch');
+            DATA.playlist = payload;
+            displayTracks();
+
+    }
+    //render();
+}
+
 // Tracks handlers
-function trackHandlers() {
+function trackHandlers(playlistId) {
+    submitSearch(playlistId);
+    addTrack(playlistId);
+    deleteTrack(playlistId);
+};
+
+function submitSearch(playlistId) {
     $('.js-tracksForm').submit(event => {
         event.preventDefault();
         let trackName = $(event.currentTarget).find('.js-newTrackName').val();
         searchTracks(trackName)
-            .then(displaySearchTracks)
-            .then(addTrack)
+            .then((data) => {
+                emit('search', data);
+            });
     });
-
-};
+}
 
 // Get track list
-function getTracks() {
+function getTracks(playlistId) {
     return new Promise((resolve, reject) => {
-        let listTracksURL = '/tracks';
+        let listTracksURL = `/playlist/${playlistId}/tracks`;
         const settings = {
             url: listTracksURL,
             dataType: 'json',
@@ -24,12 +59,14 @@ function getTracks() {
         };
         let results = $.ajax(settings);
         resolve(results)
+    }).then((playlist) => {
+        emit('fetch', playlist);
     });
 };
 
 // Search for tracks
 function searchTracks(track) {
-    return new Promise((resolve, reject) => {
+    return Promise.resolve().then(() => {
         let searchTracksURL = `/track/${track}`;
         const settings = {
             url: searchTracksURL,
@@ -37,81 +74,71 @@ function searchTracks(track) {
             type: 'GET',
             contentType: 'application/json; charset=utf-8',
         };
-        let results = $.ajax(settings);
-        resolve(results)
+        return $.ajax(settings);
+    }).then(results => {
+        DATA.results = results;
+        return results;
     });
 };
 
 // Insert track into playlist
-function addTrack(trackList) {
-    return new Promise((resolve, reject) => {
-        $('.js-searchTracks').on('click', '.js-addTrackBtn', function (event) {
-            event.preventDefault();
-            let trackId = $(event.currentTarget).parent()[0].id;
-            let selectedTrack = trackList.find((item) => item.songId === trackId);
-            let addTracksURL = '/track';
-            const settings = {
-                url: addTracksURL,
-                dataType: 'json',
-                data: JSON.stringify(selectedTrack),
-                type: 'POST',
-                contentType: 'application/json; charset=utf-8',
-            };
-            let results = $.ajax(settings);
-            resolve(results);
-        });
-    })
+function addTrack(playlistId) {
+    $('.js-searchTracks').on('click', '.js-addTrackBtn', function (event) {
+        event.preventDefault();
+        let trackId = $(event.currentTarget).parent()[0].id;
+        let selectedTrack = DATA.results.find((item) => item.songId === trackId);
+        let addTracksURL = `/playlist/${playlistId}/track`;
+        const settings = {
+            url: addTracksURL,
+            dataType: 'json',
+            data: JSON.stringify(selectedTrack),
+            type: 'POST',
+            contentType: 'application/json; charset=utf-8',
+        };
+        $.ajax(settings);
+    });
 };
 
-function deleteTrack() {
-    return new Promise((resolve, reject) => {
-        $('.js-searchTracks').on('click', '.js-deleteTrackBtn', function (event) {
-            event.preventDefault();
-            console.log('delete');
-            let trackId = $(event.currentTarget).parent()[0].id;
-            let addTracksURL = `/track/${selectedTrack.trackId}`;
-            const settings = {
-                url: addTracksURL,
-                dataType: 'json',
-                type: 'DELETE',
-                contentType: 'application/json; charset=utf-8',
-            };
-            let results = $.ajax(settings);            
-            getTracks()
-                then(displayTracks)
-        });
-    })
+function deleteTrack(playlistId) {
+    $('.js-playlist').on('submit', 'form.delete', function (event) {
+        event.preventDefault();
+        let trackId = $(event.currentTarget).parent()[0].id;
+        let addTracksURL = event.target.action;
+        const settings = {
+            url: addTracksURL,
+            dataType: 'json',
+            type: 'DELETE',
+            contentType: 'application/json; charset=utf-8',
+        };
+        $.ajax(settings).then(() => emit('delete'));
+    });
 }
 
 // Render functions
-function displayTracks(data) {
-    return new Promise((resolve, reject) => {
-        let results = data.content.map((item) => renderTrack(item)).reverse();
-        $('.js-playlist').html(results);
-        resolve(data);
-    });
+function displayTracks() {
+    let results = DATA.playlist.content.map((item) => renderTrack(item)).reverse();
+    $('.js-playlist').html(results);
 };
 
 function renderTrack(result) {
     return `
-    <br>      
+    <br>
     <br>
     <div id=${result.songId}>  
     <img src='${result.thumbnail}'>
-            <button class='js-deleteTrackBtn'>Delete Track</button>
-            <p>Song: ${result.songTitle}</p>        
-            <p>Artist: ${result.songArtist}</p>        
-            <p>Album: ${result.songAlbum}</p>                        
-        </div>
+        <form class="delete" action="/playlist/${DATA.playlistId}/track/${result.songId}">
+            <button class='js-deleteTrackBtn' type="submit">Delete Track</button>
+        </form>
+        <p>Song: ${result.songTitle}</p>        
+        <p>Artist: ${result.songArtist}</p>        
+        <p>Album: ${result.songAlbum}</p>                        
+    </div>
     `;
 };
 
-function displaySearchTracks(data) {
-    return new Promise((resolve, reject) => {
-        let results = data.map((item) => renderSearchTracks(item));
-        $('.js-searchTracks').html(results);
-        resolve(data);
-    });
+function displaySearchTracks() {
+    let results = DATA.results.map((item) => renderSearchTracks(item));
+    $('.js-searchTracks').html(results);
 };
 
 function renderSearchTracks(result) {
@@ -125,10 +152,15 @@ function renderSearchTracks(result) {
     `;
 }
 
+
 // When page loads handler
 function onPageLoad() {    
-    getTracks()
-        .then(displayTracks)        
+    // load data
+    const playlistId = DATA.playlistId;
+    getTracks(playlistId);
+
+    // setup click handlers
+    trackHandlers(playlistId);
 }
 
 $(onPageLoad());
