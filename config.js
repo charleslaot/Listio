a93f65f66a9a1763bfeee1d2ef86a1bf4755638a
krloslao <krloslao90@gmail.com>
2018-08-30 22:27:45 -0400

    Port 8080

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 
 exports.DATABASE_URL = process.env.DATABASE_URL;
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
-exports.PORT = process.env.PORT;
+exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
